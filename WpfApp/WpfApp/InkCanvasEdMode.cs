1432ff89d52a1f9ada3a66ec3e35b99baf052e28
Marta Formela <formela.marta@gmail.com>
2017-10-21 14:42:52 +0200

    inkcanvas

---
+++ WpfApp/WpfApp/InkCanvasEdMode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp
+{
+    public class InkCanvasEdMode : INotifyPropertyChanged //implementacja tego interface powoduje, że zmiany wprowadzane pojawiaj a się także na interfejsie graficznym 
+    {
+        private InkCanvasEditingMode _editMode;
+        public InkCanvasEditingMode EditMode
+        {
+            get
+            {
+                return _editMode;
+            }
+
+            set
+            {
+                _editMode = value;
+                RaiseEventPropertyChanged("EditMode");
+            }
+        }
+        public InkCanvasEdMode()
+        {
+            EditMode = InkCanvasEditingMode.Ink;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged; // event stworzony po to, żeby zapisywał zmiany 
+
+        public void RaiseEventPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        
+    }
+}
--- WpfApp/WpfApp/ink.xaml.cs
+++ WpfApp/WpfApp/ink.xaml.cs
@@ -17,11 +17,26 @@ namespace WpfApp
     /// <summary>
     /// Interaction logic for ink.xaml
     /// </summary>
+    /// 
+
     public partial class ink : Window
     {
+
+        private InkCanvasEdMode edit = new InkCanvasEdMode();
         public ink()
         {
             InitializeComponent();
+            this.DataContext = edit;
+        }
+
+        
+        private void x1_Click(object sender, RoutedEventArgs e)
+        {
+            edit.EditMode = InkCanvasEditingMode.Select;
+        }
+        private void x2_Click(object sender, RoutedEventArgs e)
+        {
+            edit.EditMode = InkCanvasEditingMode.Ink;
         }
     }
 }
