83d6ec116e40790728403c15500667e0634aa106
Marta Formela <formela.marta@gmail.com>
2017-10-21 15:57:15 +0200

    checkbox

---
+++ WpfApp/WpfApp/CheckBoxTester.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public class CheckBoxTester : INotifyPropertyChanged
+    {
+        
+
+        private bool _testChecked;
+
+        public bool TestChecked
+        {
+            get { return _testChecked; }
+            set
+            {
+                _testChecked = value;
+                RaiseEventOnChange("TestChecked");
+            }
+        }
+
+
+        public CheckBoxTester()
+        {
+            _testChecked = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaiseEventOnChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+    }
+}
---
+++ WpfApp/WpfApp/NoweOkienkoCwiczenie.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for NoweOkienkoCwiczenie.xaml
+    /// </summary>
+    public partial class NoweOkienkoCwiczenie : Window
+    {
+        private CheckBoxTester checkBoxTester = new CheckBoxTester();
+
+
+        public NoweOkienkoCwiczenie()
+        {
+            InitializeComponent();
+            this.DataContext = checkBoxTester;
+            CheckBoxStack.DataContext = checkBoxTester;
+        }
+
+        private void setTrue_Click(object sender, RoutedEventArgs e)
+        {
+            checkBoxTester.TestChecked = true;
+        }
+        private void setFalse_Click(object sender, RoutedEventArgs e)
+        {
+            checkBoxTester.TestChecked = false;
+        }
+    }
+}
