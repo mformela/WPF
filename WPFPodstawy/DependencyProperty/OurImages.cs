366468bf1b3ab9248af7e18f23d1e61091da6263
Marta Formela <formela.marta@gmail.com>
2017-10-28 14:15:03 +0200

    a

---
+++ WPFPodstawy/DependencyProperty/OurImages.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyProperty
+{
+    public class OurImage
+    {
+
+        public string PropOfImage
+        {
+            get
+            {
+                return "Default";
+            }
+        }
+
+        //private string _asd; //prywatna zmienna zawiera w nazwie "_"
+
+        public string PathOfImage { get; set; } // properties zawsze zaczynamy z dużych liter 
+        public string NameOfImage { get; set; }
+        public bool IsFound { get; set; } // tworzymy zmienną, którą wykorzystamy do stworzenia metody czy obrazek istnieje 
+        
+        
+
+
+
+        public OurImage() //nazwy metod zaczynamy z dużych
+        {
+            this.PathOfImage = "defualt.png";
+            this.NameOfImage = "Default";
+            
+            
+        }
+        
+        public OurImage(string pathOfImage, string nameOfImage) //camelcase mała a potem duże 
+        {
+            this.PathOfImage = pathOfImage;
+            this.NameOfImage = nameOfImage;
+            //tutaj sprawdzamy czy istnieje plik
+            this.IsFound = File.Exists(Path.Combine(Directory.GetCurrentDirectory(), pathOfImage));
+
+            
+           
+
+        }
+    
+       
+        
+    }
+
+   
+}
---
+++ WPFPodstawy/ImagesUserControl/ImagesWithDescription.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImagesUserControl
+{
+    /// <summary>
+    /// Interaction logic for ImagesWithDescription.xaml
+    /// </summary>
+    public partial class ImagesWithDescription : UserControl
+    {
+
+        private double _szerokosc;
+
+        public double Szerokosc
+        {
+            get
+
+            {
+                return _szerokosc;
+            }
+
+            set
+            {
+                _szerokosc = value;
+                this.Width = _szerokosc / 2;
+            }
+        }
+
+        private string _blur;
+        public string Blur
+        {
+            get
+            {
+                return _blur;
+            }
+
+            set
+            {
+               _blur = value;
+                switch(_blur)
+                {
+                    case "Off":
+                        this.blurek.Radius = 0;
+                        break;
+
+                    case "Medium":
+                        this.blurek.Radius = 2;
+                        break;
+
+                    case "Full":
+                        this.blurek.Radius = 6;
+                        break;
+                    default:
+                        this.blurek.Radius = 1;
+                        break;
+                }
+            }
+        }
+
+        
+
+
+        public ImagesWithDescription()
+        {
+            InitializeComponent();
+        }
+    }
+}
