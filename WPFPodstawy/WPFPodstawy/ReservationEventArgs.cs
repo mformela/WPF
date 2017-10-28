61973a26f4fdae89a795f695e84f6bcab540ca96
Marta Formela <formela.marta@gmail.com>
2017-10-28 09:57:48 +0200

    a

---
+++ WPFPodstawy/WPFPodstawy/ReservationEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFPodstawy
+{
+
+    //customowa klasa która bédzie nam dawaøa dodatkowe dane
+    public class ReservationEventArgs : EventArgs
+    {
+        public string ReservationID { get; private set; }
+        public  string Name { get; private set; }
+        public string Movie { get; private set; }
+
+
+
+        // Tworzymy konstruktor 
+        //guid -> statyczna metoda, ktora inicjalizuje instancje ze struktury Guid
+        public ReservationEventArgs(string name, string movie)
+        {
+            this.ReservationID = System.Guid.NewGuid().ToString();
+            this.Name = name;
+            this.Movie = movie;
+        }
+    }
+}
