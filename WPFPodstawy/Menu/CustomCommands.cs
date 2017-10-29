d255f8ab3f0e4e36842d1e451bdeeec0d0136ec0
Marta Formela <formela.marta@gmail.com>
2017-10-29 16:07:59 +0100

    a

---
+++ WPFPodstawy/Menu/CustomCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Menu
+{
+    class CustomCommands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand("Exit from App", "Exit", typeof(CutomCommands),
+            new InputGestureCollection()
+            {
+               new KeyGesture(Key.Q, ModifierKeys.Control)
+            }
+            );
+
+    }
+}
---
+++ WPFPodstawy/ProgressBar/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+
+namespace ProgressBar
+{
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WebClient webClient;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void DownloadFile(string urlAddress, string location)
+        {
+            using (webClient = new WebClient()) //wykorzystujemy using, bo jeśli pójdzie coś nie tak, to zostanie to znisczone 
+            {
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+                try
+                {
+                    webClient.DownloadFileAsync(new Uri(urlAddress), location);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBarDownload.Value = e.ProgressPercentage;
+            labelPerc.Text = e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            if(e.Cancelled == true)
+            {
+                MessageBox.Show("Pobieranie zostało przerwane", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                MessageBox.Show("Pobieranie zostało szczęśliwie ukończone", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void buttonDownload_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(urlAddress.Text))
+            {
+                DownloadFile(urlAddress.Text, Path.Combine(Directory.GetCurrentDirectory(), $"file_{DateTime.Now.ToShortTimeString()}.pdf"));
+            }
+        }
+    }
+}
