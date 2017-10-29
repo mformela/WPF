49c728f5cc4d0c655e44b4942243f5dd694ecbc7
Marta Formela <formela.marta@gmail.com>
2017-10-29 14:51:04 +0100

    data grid

--- WPFPodstawy/DataGrid/MainWindow.xaml.cs
+++ WPFPodstawy/DataGrid/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,78 @@ namespace DataGrid
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    
     public partial class MainWindow : Window
     {
+        ObservableCollection<PersonData> dataGridPerson = new ObservableCollection<PersonData>();
         public MainWindow()
         {
             InitializeComponent();
+
+            dataGridPerson.Add(new PersonData()
+            {
+                Name = "Maciek",
+                Age = 24,
+                Email = "asd@asd.pl",
+                IsSubscribedToNewsletter = true,
+                Website = new Uri("http://mila.com"),
+                Image = "user.png",
+                Decsription = "To jest wino"
+            }
+           );
+
+            dataGridPerson.Add(new PersonData()
+            {
+                Name = "Ola",
+                Age = 224,
+                Email = "asd@asd.pl",
+                IsSubscribedToNewsletter = false,
+            }
+            );
+
+            dataGridPerson.Add(new PersonData()
+            {
+                Name = "Konrad",
+                Age = 249,
+                Email = "asd@asldkpaosjd.pl"
+            }
+            );
+
+            dataGridPerson.Add(new PersonData()
+            {
+                Name = "Aleks",
+                Age = 29,
+                Email = "asd@asldkpaosjd.pl"
+            }
+            );
+
+            dataGridPerson.Add(new PersonData()
+            {
+                Name = "Konrad",
+                Age = 249,
+                Email = "asd@asldkpaosjd.pl"
+            }
+            );
+
+            dataGridPerson.Add(new PersonData()
+            {
+                Name = "Konrad",
+                Age = 249,
+                Email = "asd@asldkpaosjd.pl"
+            }
+            );
+
+            dataGridPerson.Add(new PersonData()
+            {
+                Name = "Konrad",
+                Age = 249,
+                Email = "asd@asldkpaosjd.pl"
+            }
+            );
+
+            dataGridPeople.ItemsSource = dataGridPerson;
+
         }
     }
 }
---
+++ WPFPodstawy/DataGrid/PersonData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGrid
+{
+    class PersonData
+    {
+        public enum Gender
+        {
+            Male,
+            Female,
+            None,
+            Undecided
+        }
+
+        public string Name { get; set; }
+        public int Age  { get; set; }
+        public string Email  { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public Uri Website { get; set; }
+        public Gender GenderType { get; set; }
+        public string Image { get; set; }
+        public string Decsription { get; set; }
+
+    }
+}
