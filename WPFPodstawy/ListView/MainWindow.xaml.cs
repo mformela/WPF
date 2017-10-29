fe08a1883bde2bf68a6ec217373b0025f35ec700
Marta Formela <formela.marta@gmail.com>
2017-10-29 11:34:02 +0100

    liatview

--- WPFPodstawy/ListView/MainWindow.xaml.cs
+++ WPFPodstawy/ListView/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,31 @@ namespace ListView
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+            ObservableCollection<PersonData> listOfPeople = new ObservableCollection<PersonData>();
         public MainWindow()
         {
             InitializeComponent();
+            listOfPeople.Add(new PersonData() { Name = "Anna", Age = 20, Email = "anna@gmail.com" });
+            listOfPeople.Add(new PersonData() { Name = "Łukasz", Age = 30, Email = "lukasz198@gmail.com" });
+            listOfPeople.Add(new PersonData() { Name = "Krysia", Age = 20, Email = "krysialol@gmail.com" });
+
+            listaImion.ItemsSource = listOfPeople;
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            //if (!string.IsNullOrWhiteSpace(NameToAdd.Text)) //to jest funkcja, która wyklucza możliwość wpisywania białych znaków oraz możliwości wstawiania pustych 
+            //{
+                listOfPeople.Add(new PersonData() { Name = NameToAdd.Text, Age = Convert.ToInt32(AgeToAdd.Text), Email = EmailToAdd.Text });
+
+            //inna wersja:
+            //listOfPeople.Add(new PersonData() { Name = NameToAdd.Text, Age = int.TryParse (AgeToAdd.Text, out var num) ? num : 0, Email = EmailToAdd.Text });
+
         }
     }
+    
 }
---
+++ WPFPodstawy/ListView/PersonData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListView
+{
+    class PersonData
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public static ObservableCollection<string> ItemsSource { get; internal set; }
+    }
+    
+}
